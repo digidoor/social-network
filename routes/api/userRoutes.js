9bd9fecebaaba372d5bb18364f5b0b52f2e0bb53
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-12 11:20:43 -0700

    fixed topmost users route, adding user in progress

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
-router.get('/users', async (req, res) =>
+router.get('/', async (req, res) =>
 {
 	try
 	{
 		const users = await User.find();
 		res.json(users);
+	} catch (error) { res.status(500).json(error); }
+});
+
+router.post('/:userId', async (req, res) =>
+{
+	try
+	{
+		const x = 0;
 	} catch (error) { res.status(500).json(error); }
 });
 
